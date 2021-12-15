970aee0787896fec924342d8ae13b901bbe888e8
parasharparth <91799089+parasharparth@users.noreply.github.com>
2021-12-15 04:16:46 -0800

    Add files via upload

---
+++ hw-4/03-react/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './components/NavBar'
+
+export default function App() {
+  return  <NavBar />;
+}
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <h1>Exercise 03</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
